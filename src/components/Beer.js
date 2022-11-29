70443412f57767e9818efd371b56f1dd0d926c47
capali13 <mark.csaszarnagy@gmail.com>
2022-11-29 11:30:11 +0100

    props11.29

---
+++ src/components/Beer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+/* function Beer({ beerData }) { */
+function Beer(props) {
+  console.log(props);
+  /* const { beerData } = props; */
+  const { title, sub } = props.beerData;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{sub}</p>
+    </div>
+  );
+}
+
+export default Beer;
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,7 +1,12 @@
 import "./Hello.css";
 
-function Hello() {
-  return <div className="Hello">First react component!</div>;
+function Hello(props) {
+  console.log(props);
+  return (
+    <div className="Hello">
+      I'm {props.name} and I'M {props.age}
+    </div>
+  );
 }
 
 export default Hello;
