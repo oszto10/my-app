e6356a24dcf9386342ed799626aebbc47774f7b9
capali13 <mark.csaszarnagy@gmail.com>
2022-11-30 15:24:59 +0100

    11.30

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Pizza.css";
 
-function Pizza({ name, id, ingredients, picture, prize }) {
+function Pizza({ name, id, ingredients, picture, price }) {
   /*   const pizzaName = pizzaData.name;
   const  = pizzaData */
+  const [isFavorite, setIsFavorite] = useState(false);
 
   return (
-    <div>
-      <h1>{name}</h1>
+    <div className={"Pizza" + (isFavorite ? " fav" : "")}>
+      <img
+        onClick={() => setIsFavorite(true)}
+        src="https://as1.ftcdn.net/v2/jpg/01/09/84/42/1000_F_109844239_A7MdQSDf4y1H80cfvHZuSa0zKBkZ68S7.jpg"
+        alt="csillag"
+        className="favorite"
+      />
+      <p>Sorszám: {id}</p>
+      <h1>
+        {name}
+        {isFavorite ? "Kedvenc" : "Nem kedvenc"}
+      </h1>
+      <h3>{ingredients}</h3>
+      <h1>{price} Ft</h1>
     </div>
   );
 }
--- src/components/Pizzas.js
+++ src/components/Pizzas.js
@@ -6,7 +6,13 @@ function Pizzas() {
   return (
     <div>
       {pizzas.map((pizza) => (
-        <Pizza name={pizza.name} id={pizza.id} />
+        <Pizza
+          name={pizza.name}
+          id={pizza.id}
+          ingredients={pizza.ingredients}
+          price={pizza.price}
+          key={pizza.id}
+        />
       ))}
     </div>
   );
